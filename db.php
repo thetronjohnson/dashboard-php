d18768ce43a07dce24b96b803e8f91d0f1dba2a6
Kiran Johns <kiranjohns369@gmail.com>
2020-06-17 21:47:42 +0530

    final

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+    $host = "host = 127.0.0.1";
+    $port = "port = 5432";
+    $dbname = "dbname = fosslab";
+    $credentials = "user=root password=root";
+
+    $db = pg_connect("$host $port $dbname $credentials");
+    if(!$db){
+        echo "Error: unable to create connection to db\n";
+    }
+    else{
+        echo "Success: Connected to db";
+    }
+?>
---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
+</head>
+
+<body>
+  
+  <nav class="navbar is-transparent is-link">
+
+    <div id="navbarExampleTransparentExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" href="/index.html">
+          Home
+        </a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="field is-grouped">
+
+            <p class="control">
+              <a class="button is-white" href="/index.html">
+                <span>Login</span>
+              </a>
+            </p>
+
+
+            <p class="control">
+              <a class="button is-white" href="/signup.html">
+                <span>Signup</span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+
+  <?php
+
+require('db.php');
+session_start();
+if(isset($_REQUEST['username'])){
+  $username = stripslashes($_REQUEST['username']);
+  $password = stripslashes($_REQUEST['password']);
+
+  $query = "SELECT * FROM `users` WHERE username='$username' AND  password='" . md5($password). "'";
+  $result = pg_query($db,$query);
+  $rows = pg_num_rows($result);
+
+  if($rows==1){
+      $_SESSION['username'] = $username;
+      header("Location: dashboard.php");
+  }else{
+    echo "
+    <div class='notification is-link'>
+    <strong>Incorrect Credentials</strong>
+    </div>";
+    
+  }
+
+}
+
+?>
+
+  <section class="section">
+    <div class="container">
+      <h2 class="title">Login</h2>
+      <form class="form" method="POST" name="login">
+      <div class="field">
+        <label class="label">Username</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="Enter your username" name="username">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Password</label>
+        <div class="control">
+          <input class="input" type="password" placeholder="Enter your password" name="password">
+        </div>
+      </div>
+
+
+      <div class="field is-grouped">
+        <div class="control">
+          <button class="button is-link">Login</button>
+        </div>
+      </div>
+</form>
+    </div>
+  </section>
+</body>
+
+</html>
